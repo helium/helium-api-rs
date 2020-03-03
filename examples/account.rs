d5017298879b87f599888177aecd7ec52d0118ac
Louis Thiery <louis@helium.com>
2020-03-03 09:34:21 -0800

    fix client in examples

--- examples/account.rs
+++ examples/account.rs
@@ -1,7 +1,7 @@
 use helium_api::Client;
 
 fn main() {
-    let client = Client::new();
+    let client = Client::default();
     let account = client.get_account("13buBykFQf5VaQtv7mWj2PBY9Lq4i1DeXhg7C4Vbu3ppzqqNkTH");
     println!("Account: {:?}", account);
 }
--- examples/hotspot.rs
+++ examples/hotspot.rs
@@ -1,7 +1,7 @@
 use helium_api::Client;
 
 fn main() {
-    let client = Client::new();
+    let client = Client::default();
     let hotspot = client.get_hotspot("11VKaN7fEvDm6NaGhcZtNSU1KAQQmTSwuuJsYYEqzh8mSWkoEUd");
     println!("Hotspot: {:?}", hotspot);
     let hotspots = client.get_hotspots("13buBykFQf5VaQtv7mWj2PBY9Lq4i1DeXhg7C4Vbu3ppzqqNkTH");
