4e195cfb4a93a19d2c4cc30487c1c96ecf867246
Chris Bruce <chris@chrisbruce.com>
2020-04-14 14:54:58 +0200

    fixes fmt

--- examples/height.rs
+++ examples/height.rs
@@ -4,6 +4,6 @@ fn main() {
     let client = Client::default();
     match client.get_height() {
         Ok(height) => println!("Block Height: {}", height),
-        Err(e) => println!("Error fetching block height: {:?}", e)
+        Err(e) => println!("Error fetching block height: {:?}", e),
     }
 }
