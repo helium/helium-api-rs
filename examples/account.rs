dfa5b44df466a4d579aca89701e904e85232e298
Louis Thiery <thiery.louis@gmail.com>
2020-09-10 10:15:22 -0700

    support cursors and upgrade examples

--- examples/account.rs
+++ examples/account.rs
@@ -2,6 +2,27 @@ use helium_api::Client;
 
 fn main() {
     let client = Client::default();
-    let account = client.get_account("13buBykFQf5VaQtv7mWj2PBY9Lq4i1DeXhg7C4Vbu3ppzqqNkTH");
-    println!("Account: {:?}", account);
+    let address = "13buBykFQf5VaQtv7mWj2PBY9Lq4i1DeXhg7C4Vbu3ppzqqNkTH";
+
+    match client.get_account(address) {
+        Ok(account) => {
+            println!("{:?}", account);
+            match client.get_account_transactions(address) {
+                Ok((transactions, _cursor)) => {
+                    if let Some(transactions) = transactions {
+                        println!("Transactions: ");
+                        for txn in transactions {
+                            println!("{:?}", txn);
+                        }
+                    }
+                }
+                Err(e) => {
+                    println!("Error querying account {} transactions: {:?}", address, e);
+                }
+            }
+        }
+        Err(e) => {
+            println!("Error querying account {}: {:?}", address, e);
+        }
+    }
 }
