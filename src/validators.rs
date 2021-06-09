1c4b98f2080fc0bfc63901d5e16547dfe41e142e
Chris Bruce <chris@lumeo.com>
2021-06-09 11:53:29 +0200

    adds validator rewards

--- src/validators.rs
+++ src/validators.rs
@@ -1,4 +1,5 @@
 use crate::*;
+use chrono::{DateTime, Utc};
 use serde::{Deserialize, Serialize};
 
 #[derive(Clone, Serialize, Deserialize, Debug)]
@@ -41,6 +42,34 @@ pub struct StakeStats {
     pub count: u64,
 }
 
+/// Reward for validator
+#[derive(Clone, Debug, Deserialize, Serialize)]
+pub struct Reward {
+    /// The owner address is the base58 check-encoded public key of
+    /// the owner's wallet address.
+    pub account: String,
+    /// The reward amount.
+    pub amount: i64,
+    /// The block the reward was earned in.
+    pub block: i64,
+    /// The validator address is the base58 check-encoded public key of
+    /// the validator.
+    pub gateway: String,
+    /// The transaction hash of the reward.
+    pub hash: String,
+    /// The timestamp of the rewards.
+    pub timestamp: DateTime<Utc>,
+}
+
+/// Query params for requests that can take in a time range.
+#[derive(Clone, Debug, Serialize)]
+pub struct QueryTimeRange {
+    /// ISO 8601 timestamp or relative time (-3 hour) minimum time range
+    pub min_time: String,
+    /// ISO 8601 timestamp or relative time (-3 hour) maximum time range
+    pub max_time: String,
+}
+
 /// Get all known validators
 pub fn all(client: &Client) -> Stream<Validator> {
     client.fetch_stream("/validators", NO_QUERY)
@@ -56,4 +85,13 @@ pub async fn get(client: &Client, address: &str) -> Result<Validator> {
 /// Get stats for validators
 pub async fn stats(client: &Client) -> Result<Stats> {
     client.fetch("/validators/stats", NO_QUERY).await
+}
+
+/// Get reward total for a validator
+///
+/// Returns rewards for a given validator per reward block the validator is in,
+/// for a given timeframe. `QueryTimeRange` contains the timestamps given in ISO 8601 format, or in relative
+/// time. The block that contains the max_time timestamp is excluded from the result.
+pub async fn rewards(client: &Client, address: &str, query: &QueryTimeRange) -> Stream<Reward> {
+    client.fetch_stream(&format!("/validators/{}/rewards", address), query)
 }
