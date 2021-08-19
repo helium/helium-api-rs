96d98a8f58c717ad52c560f8818b72286dfa1d9a
Chris Bruce <chris@chrisbruce.com>
2021-08-19 14:49:18 +0200

    integrate louis' changes into transaction handling

--- src/models/transaction.rs
+++ src/models/transaction.rs
@@ -1,10 +1,11 @@
+// Copied from: https://github.com/dewi-alliance/helium-jsonrpc-rs/commit/70d7e6e9202f6ee00eae72274d22e1d641575219
+
 use serde::{Deserialize, Serialize};
 
 #[derive(Clone, Deserialize, Debug)]
 pub struct PendingTxnStatus {
     pub hash: String,
 }
-
 #[derive(Clone, Serialize, Deserialize, Debug)]
 pub struct PaymentV2Payment {
     pub amount: u64,
@@ -18,7 +19,7 @@ pub struct Witness {
     pub datarate: String,
     pub frequency: f64,
     pub gateway: String,
-    pub is_valid: bool,
+    pub is_valid: Option<bool>,
     pub packet_hash: String,
     pub signal: i64,
     pub snr: f64,
@@ -54,10 +55,12 @@ pub struct Reward {
 }
 
 #[derive(Clone, Serialize, Deserialize, Debug)]
-pub struct RoutingAction {
-    pub index: u64,
-    pub action: String,
-    pub filter: String,
+#[serde(tag = "action", rename_all = "snake_case")]
+pub enum RoutingAction {
+    NewXor { filter: String },
+    UpdateXor { filter: String, index: usize },
+    UpdateRouters { addresses: Vec<String> },
+    RequestSubnet { requested_subnet_size: u64 },
 }
 
 #[derive(Clone, Serialize, Deserialize, Debug)]
@@ -70,6 +73,15 @@ pub enum Transaction {
         height: u64,
         members: Vec<String>,
         proof: String,
+    },
+    ConsensusGroupFailureV1 {
+        delay: u64,
+        hash: String,
+        block: u64,
+        height: u64,
+        members: Vec<String>,
+        failed_members: Vec<String>,
+        signatures: Vec<String>,
     },
     PocRequestV1 {
         hash: String,
@@ -269,6 +281,28 @@ pub enum Transaction {
         stake: u64,
         owner_signature: String,
     },
+    TransferValidatorStakeV1 {
+        block: u64,
+        fee: u64,
+        hash: String,
+        new_address: String,
+        new_owner: String,
+        new_owner_signature: Option<String>,
+        old_address: String,
+        old_owner: String,
+        old_owner_signature: String,
+        payment_amount: u64,
+        stake_amount: u64,
+    },
+    UnstakeValidatorV1 {
+        address: String,
+        owner: String,
+        owner_signature: String,
+        fee: u64,
+        stake_amount: u64,
+        stake_release_height: u64,
+        hash: String,
+    },
     // no examples found on blockchain. inferred from proto source code
     CoinbaseV1 {
         hash: String,
@@ -296,7 +330,6 @@ pub enum Transaction {
     #[serde(other)]
     Unknown, // Any other transaction not supported
 }
-
 #[cfg(test)]
 mod test {
     use super::*;
@@ -309,11 +342,10 @@ mod test {
         let txn = transactions::get(&client, "1gidN7e6OKn405Fru_0sGhsqca3lTsrfGKrM4dwM_E8")
             .await
             .expect("PocRequestV1");
-        match txn {
-            Transaction::PocRequestV1 { block_hash, .. } => {
-                assert_eq!(block_hash, "RS2mBvd_4pbKCglkkyMroDQekPNO0xDdYx6Te3HGDGg")
-            }
-            _ => (),
+        if let Transaction::PocRequestV1 { block_hash, .. } = txn {
+            assert_eq!(block_hash, "RS2mBvd_4pbKCglkkyMroDQekPNO0xDdYx6Te3HGDGg")
+        } else {
+            assert!(false)
         }
     }
     #[test]
@@ -322,11 +354,10 @@ mod test {
         let txn = transactions::get(&client, "yh01SJk8dvyqb-BGXxkHFUuLi6wF1pfL0VEFStJUt-E")
             .await
             .expect("ConsensusGroupV1");
-        match txn {
-            Transaction::ConsensusGroupV1 { hash, .. } => {
-                assert_eq!(hash, "yh01SJk8dvyqb-BGXxkHFUuLi6wF1pfL0VEFStJUt-E")
-            }
-            _ => panic!("Didn't find ConsensusGroupV1"),
+        if let Transaction::ConsensusGroupV1 { hash, .. } = txn {
+            assert_eq!(hash, "yh01SJk8dvyqb-BGXxkHFUuLi6wF1pfL0VEFStJUt-E")
+        } else {
+            assert!(false)
         }
     }
     #[test]
@@ -336,9 +367,10 @@ mod test {
         let txn = transactions::get(&client, "C_jJZLKBOv_gRQ6P6wEpZPiRVAjf44FOx1iHOFD4haA")
             .await
             .expect("PaymentV2");
-        match txn {
-            Transaction::PaymentV2 { payments, .. } => assert_eq!(payments.len(), 1),
-            _ => (),
+        if let Transaction::PaymentV2 { payments, .. } = txn {
+            assert_eq!(payments.len(), 1)
+        } else {
+            assert!(false)
         }
     }
     #[test]
@@ -347,24 +379,23 @@ mod test {
         let txn = transactions::get(&client, "8RaF-G4pvMVuIXfBYhdqNuIlFSEHPm_rC8TH-h4JYdE")
             .await
             .expect("PocReceipt");
-        match txn {
-            Transaction::PocReceiptsV1 { hash, .. } => {
-                assert_eq!(hash, "8RaF-G4pvMVuIXfBYhdqNuIlFSEHPm_rC8TH-h4JYdE")
-            }
-            _ => (),
+        if let Transaction::PocReceiptsV1 { hash, .. } = txn {
+            assert_eq!(hash, "8RaF-G4pvMVuIXfBYhdqNuIlFSEHPm_rC8TH-h4JYdE")
+        } else {
+            assert!(false)
         }
     }
+
     #[test]
     async fn payment_v1() {
         let client = Client::default();
         let txn = transactions::get(&client, "iMSckt_hUcMFY_d7W-QOupY0MGq_g3-CC2dq3P-HWIw")
             .await
             .expect("PaymentV1");
-        match txn {
-            Transaction::PaymentV1 { payee, .. } => {
-                assert_eq!(payee, "14YeKFGXE23yAdACj6hu5NWEcYzzKxptYbm5jHgzw9A1P1UQfMv")
-            }
-            _ => (),
+        if let Transaction::PaymentV1 { payee, .. } = txn {
+            assert_eq!(payee, "14YeKFGXE23yAdACj6hu5NWEcYzzKxptYbm5jHgzw9A1P1UQfMv")
+        } else {
+            assert!(false)
         }
     }
     #[test]
@@ -373,9 +404,10 @@ mod test {
         let txn = transactions::get(&client, "X0HNRGZ1HAX51CR8qS6LTopAosjFkuaaKXl850IpNDE")
             .await
             .expect("RewardsV2");
-        match txn {
-            Transaction::RewardsV2 { rewards, .. } => assert_eq!(rewards.len(), 10138),
-            _ => (),
+        if let Transaction::RewardsV2 { rewards, .. } = txn {
+            assert_eq!(rewards.len(), 10138)
+        } else {
+            assert!(false)
         }
     }
     #[test]
@@ -384,11 +416,10 @@ mod test {
         let txn = transactions::get(&client, "_I16bycHeltuOo7eyqa4uhv2Bc7awcztZflyvRkVZ24")
             .await
             .expect("AssertLocationV1");
-        match txn {
-            Transaction::AssertLocationV1 { hash, .. } => {
-                assert_eq!(hash, "_I16bycHeltuOo7eyqa4uhv2Bc7awcztZflyvRkVZ24")
-            }
-            _ => (),
+        if let Transaction::AssertLocationV1 { hash, .. } = txn {
+            assert_eq!(hash, "_I16bycHeltuOo7eyqa4uhv2Bc7awcztZflyvRkVZ24")
+        } else {
+            assert!(false)
         }
     }
     #[test]
@@ -397,12 +428,13 @@ mod test {
         let txn = transactions::get(&client, "TfjRv733Q9FBQ1_unw1c9g5ewVmMBuyf7APuyxKEqrw")
             .await
             .expect("AssertLocationV2");
-        match txn {
-            Transaction::AssertLocationV2 { gateway, .. } => assert_eq!(
+        if let Transaction::AssertLocationV2 { gateway, .. } = txn {
+            assert_eq!(
                 gateway,
                 "112WVxXCrCjiKmmDXLDUJuhYGEHMbXobUZe8oJQkHoMHEFa149a"
-            ),
-            _ => (),
+            )
+        } else {
+            assert!(false)
         }
     }
     #[test]
@@ -411,26 +443,106 @@ mod test {
         let txn = transactions::get(&client, "aoTggHSgaBAamuUUrXnY42jDZ5WUBxE0k-tshvfn35E")
             .await
             .expect("AddGatewayV1");
-        match txn {
-            Transaction::AddGatewayV1 { gateway, .. } => assert_eq!(
+        if let Transaction::AddGatewayV1 { gateway, .. } = txn {
+            assert_eq!(
                 gateway,
                 "112uuvztDziVQyLVvBxMsovsSPV5ZXkN6uQ5hrWSaWwV1oEZTZtd"
-            ),
-            _ => (),
+            )
+        } else {
+            assert!(false)
         }
     }
+
+    #[test]
+    async fn add_gateway_v1_error() {
+        let client = Client::default();
+        let txn = transactions::get(&client, "ia3c386ZnlVJorvo60WtXEFxy_0w35ImoIdmnW5lpJ8")
+            .await
+            .expect("AddGatewayV1");
+        if let Transaction::AddGatewayV1 { gateway, .. } = txn {
+            assert_eq!(
+                gateway,
+                "11GPcDmZGniewReZCnpC3SM19Jcw1sQU8W8CUnd7TCh4A6RmEkV"
+            )
+        } else {
+            assert!(false)
+        }
+    }
+
     #[test]
     async fn transfer_hotspot_v1() {
         let client = Client::default();
         let txn = transactions::get(&client, "fSFua7A8G41K05QXAvJi5N2OB0QqmQ7xp7u-My4rYHc")
             .await
             .expect("TransferHotspotV1");
-        match txn {
-            Transaction::TransferHotspotV1 { seller, .. } => assert_eq!(
+        if let Transaction::TransferHotspotV1 { seller, .. } = txn {
+            assert_eq!(
                 seller,
                 "14mo9fFGKYFaWh7xscpDLg7misWcuU5xqR8mc8gHr4c43nDnzeX"
-            ),
-            _ => (),
+            )
+        } else {
+            assert!(false)
+        }
+    }
+
+    #[test]
+    async fn transfer_routing_v1_new_xor() {
+        let client = Client::default();
+
+        let txn = transactions::get(&client, "EjL6nBsSxovJluW-kdAaPcEiRt0OPIATOmlHD1Lth4Y")
+            .await
+            .expect("RoutingV1_NewXor");
+
+        if let Transaction::RoutingV1 {
+            fee: _,
+            oui,
+            hash: _,
+            nonce: _,
+            owner,
+            action,
+        } = txn
+        {
+            assert_eq!(oui, 12);
+            assert_eq!(
+                owner,
+                "112ewJNEUfSg3Jvo276tMjzFC2JzmmZcJJ32CWz2fzYqbyCMMTe1",
+            );
+            if let RoutingAction::NewXor { filter } = action {
+                assert_eq!(filter, "wVwCiewtCpELAAAAAAAAAAAAAAAAAAAAf2gAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA");
+            } else {
+                assert!(false)
+            }
+        } else {
+            assert!(false)
+        }
+    }
+
+    #[test]
+    async fn unstake_validator_v1() {
+        let client = Client::default();
+
+        let txn = transactions::get(&client, "fMT-7_f2WQNKAYIQWX2-V258KsoI61HYbt_zAbN3A1I")
+            .await
+            .expect("UnstakeValidatorv1");
+
+        if let Transaction::UnstakeValidatorV1 {
+            address,
+            owner: _owner,
+            owner_signature: _own_signature,
+            fee,
+            stake_amount,
+            stake_release_height: _stake_release_height,
+            hash: _hash,
+        } = txn
+        {
+            assert_eq!(
+                address,
+                "11cY9Ly5H3hU4Ai2k7G9niHLAxsKb1ragQYGLJ7E9vh4Vnx6Efb"
+            );
+            assert_eq!(stake_amount, 1000000000000);
+            assert_eq!(fee, 35000);
+        } else {
+            assert!(false)
         }
     }
 }
