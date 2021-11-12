2c7d18bdb1af1e239ee6b7e3f4eab420731f93f3
Abhay Kumar <abhay@helium.com>
2021-11-11 17:16:44 -0800

    Add staked_balance, some nonces, and reorder

--- src/models/account.rs
+++ src/models/account.rs
@@ -11,16 +11,23 @@ pub struct Account {
     /// The latest balance of the wallet known to the API
     #[serde(deserialize_with = "Hnt::deserialize")]
     pub balance: Hnt,
-    /// The data credit balance of the wallet known to the API
-    pub dc_balance: u64,
-    /// The security token balance of the wallet known to the API
-    #[serde(deserialize_with = "Hst::deserialize")]
-    pub sec_balance: Hst,
+    /// The latest staked balance of the wallet known to the API
+    #[serde(deserialize_with = "Hnt::deserialize")]
+    pub staked_balance: Hnt,
     /// The current nonce for the account
     pub nonce: u64,
     /// The speculative nonce for the account
     #[serde(default)]
     pub speculative_nonce: u64,
+    /// The data credit balance of the wallet known to the API
+    pub dc_balance: u64,
+    /// The current data credit nonce for the account
+    pub dc_nonce: u64,
+    /// The security token balance of the wallet known to the API
+    #[serde(deserialize_with = "Hst::deserialize")]
+    pub sec_balance: Hst,
+    /// The current security nonce for the account
+    pub sec_nonce: u64,
     /// The speculative security nonce for the account
     #[serde(default)]
     pub speculative_sec_nonce: u64,
