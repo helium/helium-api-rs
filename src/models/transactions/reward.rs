329e1d52107640bccb7ce485128181ddd5d0a835
lthiery <thiery.louis@gmail.com>
2022-03-09 10:16:28 -0800

    fix reward type to reflect Hnt

--- src/models/transactions/reward.rs
+++ src/models/transactions/reward.rs
@@ -1,9 +1,10 @@
+use crate::models::Hnt;
 use serde::{Deserialize, Serialize};
 
 #[derive(Clone, Serialize, Deserialize, Debug)]
 pub struct Reward {
     pub account: Option<String>,
-    pub amount: u64,
+    pub amount: Hnt,
     pub gateway: Option<String>,
     pub r#type: String,
 }
