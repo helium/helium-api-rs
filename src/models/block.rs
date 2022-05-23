a4db8b9b6b04add6e3dd103b8bbcf13452b262da
Trevor Arjeski <trevor.arjeski@crypto.com>
2022-05-23 20:40:34 +0300

    Add block descriptions API call
    Extended the block module with a function to retrieve recent block descriptions from the blockchain.

--- src/models/block.rs
+++ src/models/block.rs
@@ -1,7 +1,23 @@
 use serde::Deserialize;
 
-#[derive(Deserialize)]
+#[derive(Debug, Deserialize)]
 pub struct Height {
     /// The current block height of the chain.
     pub height: u64,
+}
+
+#[derive(Debug, Deserialize)]
+pub struct Descriptions {
+    pub data: Vec<BlockData>,
+    pub cursor: Option<String>,
+}
+
+#[derive(Debug, Deserialize)]
+pub struct BlockData {
+    pub transaction_count: u64,
+    pub time: u64,
+    pub snapshot_hash: String,
+    pub prev_hash: String,
+    pub height: u64,
+    pub hash: String,
 }
